7cf60bb4a24918a63b07eb0d6ffa014bf7891615
Flavjack <flavjack@gmail.com>
2020-03-08 22:56:50 +0100

    doc str

--- cnfg/setup.r
+++ cnfg/setup.r
@@ -5,23 +5,23 @@
 packages <- c(
   "devtools", # For install packages and complements from dev sources
   "bookdown", # Required for use the present template
-  "citr",  # Use Zotero for citations
+  "citr",  # Use zotero for citations
   "knitr", # Base of the bookdown template
   "kableExtra", # Table notes!
   "inserttable", # Insert table with copy and paste
   "tidyverse", # Data manipulation
   "googlesheets4", # Read/write google sheets files
-  "googledrive", # Dowload files from googledrive
+  "googledrive", # Download files from googledrive
   "readxl", # Read excel files
-  "agricolae", # Agriculture data analisys
-  "GerminaR", # Germination analisys and line and bar easy plots
-  "FactoMineR", # Multivariate data anallisys
-  "cowplot", # For export and mergue figures 
-  "grid", # For mergue figures 
+  "agricolae", # Agriculture data analysis
+  "GerminaR", # Germination analysis and line and bar easy plots
+  "gtsummary", # Tables for compare treatments 
+  "FactoMineR", # Multivariate data analysis
+  "heatmaply", # Correlation plot
+  "cowplot", # For export and merge figures 
+  "grid", # For merge figures 
   "png", # Import png files
-  "jpeg", # Import jpeg files
-  "heatmaply",
-  "gtsummary" # Tables for compare treatments
+  "jpeg" # Import jpg files
 )
 
 installed_packages <- packages %in% rownames(installed.packages())
@@ -32,7 +32,17 @@ invisible(lapply(packages, library, character.only = TRUE))
 rm(packages, installed_packages)
   
 # devtools::install_github("lbusett/insert_table") # Insert table with copy and paste (Not in CRAN)
-# devtools::install_github("Flavjack/GerminaR") # Germination analisys and line and bar easy plots
+# devtools::install_github("Flavjack/GerminaR") # Germination analysis and line and bar easy plots
+
+# -------------------------------------------------------------------------
+# yml files ---------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+if(!file.exists("_output.yml"))
+  {file.copy(from = "cnfg/output.yml", to = "_output.yml")}
+
+if(!file.exists("_bookdown.yml"))
+  {file.copy(from = "cnfg/bookdown.yml", to = "_bookdown.yml")}
 
 # -------------------------------------------------------------------------
 # Knitr options -----------------------------------------------------------
@@ -40,7 +50,7 @@ rm(packages, installed_packages)
 
 knitr::opts_chunk$set(
   fig.align = "center", # Center images in the export file
-  out.width = "98%", # Figure width in htlm
+  out.width = "98%", # Figure width in html
 	echo = FALSE, # Avoid print code in the export file
   message = FALSE,  # Avoid print messages in the export file
   warning = FALSE # Avoid print messages in the export file
@@ -52,12 +62,13 @@ knitr::opts_chunk$set(
 
 options(
   digits = 4, # Number of digits in the values
-  OutDec= ".", # Use "." insted of "," in the decimal values
+  OutDec= ".", # Use "." instead of "," in the decimal values
   scipen = 99, # Avoid use "6e-04"
   knitr.kable.NA = "", # NA values will appear as empty cell
   knitr.table.format = "pandoc", # Format for export tables
   citr.use_betterbiblatex = FALSE, # For zotero addin 
-  gargle_oob_default = TRUE # Googlesheets4 auth process
+  gargle_oob_default = TRUE, # googlesheets auth process
+  gtsummary.print_engine = "kable"
   ) 
 
 # -------------------------------------------------------------------------
@@ -74,6 +85,8 @@ if(!dir.exists("files"))
   {dir.create("files")}
 if(dir.exists("docs"))
   {zip::zipr(zipfile = "docs/files.zip", files = "files")}
+if(dir.exists("docs"))
+  {unlink(c("_book", "_main.Rmd"), recursive = T)}
 
 # -------------------------------------------------------------------------
 # authorize googledrive & googlesheets ------------------------------------
